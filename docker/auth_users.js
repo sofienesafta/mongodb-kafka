fa2b19e5e70b1fdc61cae6dd5ec79f79e387fa4b
sofienesafta <97222999+sofienesafta@users.noreply.github.com>
2022-06-08 17:54:13 +0100

    Add files via upload

---
+++ docker/auth_users.js
@@ -0,0 +1,21 @@
+use admin
+db.auth("root","root")
+
+use patient
+db.createUser({user:"doctor", pwd:"doctor", roles: [{role: "read", db:"patient"}]})
+
+db.createRole( {role:"writecollDoctor", privileges: [ {resource:{db: "patient", collection:"medical_action"},
+  actions: ["find", "update", "insert", "remove",  "renameCollectionSameDB","dropCollection","createIndex"]}],roles:[] })
+
+db.grantRolesToUser("doctor",["writecollDoctor"])
+
+
+
+db.createUser({user:"care_maker", pwd:"care_maker", roles:[]})
+
+
+db.createRole({role:"caremakerRole", privileges: [ {resource:{db: "patient", collection:"sensorlogs"},
+  actions: ["find","collStats"]}, {resource:{db: "patient", collection:"medical_action"},
+  actions: ["find", "update", "insert", "remove"]} ],roles:[]})
+
+db.grantRolesToUser("care_maker",["caremakerRole"])
