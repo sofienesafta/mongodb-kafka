cf640ecf6e99c8094b7a4961ab076e5b88eb78a6
sofienesafta <97222999+sofienesafta@users.noreply.github.com>
2022-07-08 16:53:34 +0100

    Update alert_nurse.py

--- alert_nurse.py
+++ alert_nurse.py
@@ -3,14 +3,14 @@ from json import loads
 from email.message import EmailMessage
 import smtplib
 
-def email_alert(subject,body,to):
+def email_alert(subject,body,to,passw):
     msg= EmailMessage()
     msg.set_content(body)
     msg['subject']= subject
     msg['to'] = to
     user = "sofiene.safta19@gmail.com"
     msg['from'] = user
-    password= "jtrcanuaytrfxtjr"
+    password= passw
     server = smtplib.SMTP("smtp.gmail.com",587)
     server.starttls()
     server.login(user, password)
