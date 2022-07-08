ac73101d884a6f3ef186575401420c762e807e99
sofienesafta <97222999+sofienesafta@users.noreply.github.com>
2022-07-08 17:45:20 +0100

    Update alert_nurse.py

--- alert_nurse.py
+++ alert_nurse.py
@@ -3,13 +3,13 @@ from json import loads
 from email.message import EmailMessage
 import smtplib
 
-def email_alert(subject,body,to,passw):
+def email_alert(subject,body,to,user_address,passw):
     msg= EmailMessage()
     msg.set_content(body)
     msg['subject']= subject
     msg['to'] = to
-    user = "sofiene.safta19@gmail.com"
-    msg['from'] = user
+    user = user
+    msg['from'] = user_address
     password= passw
     server = smtplib.SMTP("smtp.gmail.com",587)
     server.starttls()
@@ -23,7 +23,9 @@ consumer = KafkaConsumer('urgent_data',
          group_id = 'Alert_staff'
                         ) 
 
-staff_email = input("Email : " )   ## The user gives a valid email to test Alert message.
+staff_email = input("to : " )   ## The user gives a valid email to test Alert message.
+user_address= input(" user : ")
+passw= input("password : " ) ## The user has to contact me
 for msg in consumer:
     print("ALERT Emergency")    
-    email_alert("ALERT emergency","A patient need urgent intervention" ,staff_email)
+    email_alert("ALERT emergency","A patient need urgent intervention" ,staff_email,user_address,passw)
