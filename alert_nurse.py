940df64d812cc2058de55623597fc538b750b584
sofienesafta <97222999+sofienesafta@users.noreply.github.com>
2022-07-08 14:05:37 +0100

    Update alert_nurse.py

--- alert_nurse.py
+++ alert_nurse.py
@@ -8,9 +8,9 @@ def email_alert(subject,body,to):
     msg.set_content(body)
     msg['subject']= subject
     msg['to'] = to
-    user = "sofiene.safta@horizon-tech.tn"
+    user = "sofiene.safta19@gmail.com"
     msg['from'] = user
-    password= "lbdpyemcsksjrsgk"
+    password= "jtrcanuaytrfxtjr"
     server = smtplib.SMTP("smtp.gmail.com",587)
     server.starttls()
     server.login(user, password)
@@ -20,10 +20,10 @@ def email_alert(subject,body,to):
 # create the Kafka Consumer  
 consumer = KafkaConsumer('urgent_data',
          bootstrap_servers = ['localhost : 9092'], 
-         group_id = 'care_makers'
+         group_id = 'Alert_staff'
                         ) 
 
-nurse_email = input("Email : " )   ## The user gives a valid email to test Alert message.
+staff_email = input("Email : " )   ## The user gives a valid email to test Alert message.
 for msg in consumer:
     print("ALERT Emergency")    
-    email_alert("ALERT emergency","A patient need urgent intervention" ,nurse_email)
+    email_alert("ALERT emergency","A patient need urgent intervention" ,staff_email)
